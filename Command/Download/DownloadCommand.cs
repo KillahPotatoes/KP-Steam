d40fa66d8ad4a6daa29efa6644101738c7f586a7
Filip Maciejewski <veteran29q@gmail.com>
2019-08-27 21:50:03 +0200

    Add DownloadItem command

--- Command/Download/DownloadCommand.cs
+++ Command/Download/DownloadCommand.cs
@@ -2,17 +2,18 @@ using System;
 using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Logging;
+using Steamworks;
 
 namespace KP_Steam_Uploader.Command.Download
 {
-    [Command(Name = "download", Description = "Download Steam Workshop item")]
+    [Command(Name = "download", Description = "Queues Steam Workshop item for download without subscribing the item.")]
     public class DownloadCommand: AbstractKpSteamCommand
     {
         [Option(CommandOptionType.SingleValue, ShortName = "a", LongName = "app", Description = "Steam AppId")]
-        public int AppId { get; set; }
+        public uint AppId { get; set; }
         
         [Option(CommandOptionType.SingleValue, ShortName = "i", LongName = "item", Description = "Workshop Id of item to download")]
-        public int ItemId { get; set; }
+        public ulong ItemId { get; set; }
 
         public DownloadCommand(ILogger<DownloadCommand> logger, IConsole console)
         {
@@ -23,9 +24,21 @@ namespace KP_Steam_Uploader.Command.Download
         protected override async Task<int> OnExecute(CommandLineApplication app)
         {
             Logger.LogInformation($"Downloading item {ItemId} for app {AppId}");
+            Console.Out.WriteLine($"Downloading item {ItemId} for app {AppId}");
+
             try
             {
-                throw new System.NotImplementedException();
+                InitializeSteam();
+                WriteSteamAppId(AppId);
+                
+                if (!SteamUGC.DownloadItem(new PublishedFileId_t(ItemId), true))
+                {
+                    throw new Exception($"Could not download item {ItemId}");
+                }
+                
+                Console.Out.WriteLine($"Item {ItemId} queued for download in Steam");
+
+                return 0;
             }
             catch (Exception ex)
             {
